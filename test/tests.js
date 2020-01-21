05497a8bb1f23f071abf72f84e0c3b0ad9f3c330
Damien Hampton <damien.hampton@gmail.com>
2020-01-21 10:43:27 +0000

    Made test actually fail - doh!

--- test/tests.js
+++ test/tests.js
@@ -2,9 +2,9 @@ const assert = require('chai').assert;
 
 describe('Passing test', () => {
   it('should add 1 and 1', () => {
-    assert(1+1, 2, '1 + 1 should equal 2');
+    assert.equal(1+1, 2, '1 + 1 should equal 2');
   })
   it('should add 1 and 2', () => {
-    assert(1+2, 4, '1 + 2 should equal 4 - doh!');
+    assert.equal(1+2, 4, '1 + 2 should equal 4 - doh!');
   })
 })
