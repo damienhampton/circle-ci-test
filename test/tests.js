2c8667e37b8b5d01572f97727f266fa4a39b465c
Damien Hampton <damien.hampton@gmail.com>
2020-01-21 10:41:57 +0000

    Added failing test

--- test/tests.js
+++ test/tests.js
@@ -4,4 +4,7 @@ describe('Passing test', () => {
   it('should add 1 and 1', () => {
     assert(1+1, 2, '1 + 1 should equal 2');
   })
+  it('should add 1 and 2', () => {
+    assert(1+2, 4, '1 + 2 should equal 4 - doh!');
+  })
 })
